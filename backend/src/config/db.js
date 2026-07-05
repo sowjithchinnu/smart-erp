ac35fc5a079798979e487feb53d19500974875ae
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-07-05 12:32:53 +0530

    added connection string

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,19 +1,15 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 pool.connect()
-  .then(() => {
-    console.log("✅ PostgreSQL Connected");
-  })
-  .catch((err) => {
-    console.log("Database connection failed:", err.message);
-  });
+  .then(() => console.log("✅ PostgreSQL Connected"))
+  .catch((err) => console.log("Database connection failed:", err.message));
 
 module.exports = pool;
